4c9b7ff4dff86c6e836ba1660b779c2b8b4dd065
habibur <va.habibur@gmail.com>
2023-04-20 11:59:26 +0600

    finish 61.2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className='form-container'>
             <div className='form-wraper'>
-                <h3>Please Login</h3>
+                <h3 className='form-title'>Login</h3>
                 <form action="">
-                    <div>
+                    <div className='form-control'>
                         <label htmlFor="">Email</label>
                         <br />
                         <input
                             type="text"
                             placeholder='Your email'
-
+                            name='email'
+                            required
                         />
                     </div>
+                    <div className='form-control'>
+                        <label htmlFor="">Password</label>
+                        <br />
+                        <input
+                            type="password"
+                            placeholder='Your email'
+                            name='password'
+                            required
+                        />
+                    </div>
+                    <input className='submit-btn' type="submit" value="Login" />
                 </form>
+                <p className='redirect-link'>New to Ema Jhon? <Link to='/signup'>Create and account</Link></p>
+                <button className='submit-btn google-btn' type="submit" value="Login">Google</button>
             </div>
         </div>
     );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import './SignUp.css'
+import { Link } from 'react-router-dom';
 const SignUp = () => {
     return (
-        <div>
-            <h3>Signup</h3>
+        <div className='form-container'>
+            <div className='form-wraper'>
+                <h3 className='form-title'>Signup</h3>
+                <form action="">
+                    <div className='form-control'>
+                        <label htmlFor="">Email</label>
+                        <br />
+                        <input
+                            type="text"
+                            placeholder='Your email'
+                            name='email'
+                            required
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor="">Password</label>
+                        <br />
+                        <input
+                            type="password"
+                            placeholder='Your email'
+                            name='password'
+                            required
+                        />
+                    </div>
+                    <div className='form-control'>
+                        <label htmlFor=""> Confirm Password</label>
+                        <br />
+                        <input
+                            type="password"
+                            placeholder='Your email'
+                            name='password'
+                            required
+                        />
+                    </div>
+                    <input className='submit-btn' type="submit" value="SignUp" />
+                </form>
+                <p className='redirect-link'>Already have an account? <Link to='/login'>Login</Link></p>
+                <button className='submit-btn google-btn' type="submit" value="Login">Google</button>
+            </div>
         </div>
     );
 };
